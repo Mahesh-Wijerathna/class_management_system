c8b019cd929997ee8481ba3a9319247f8c36296e
Bawantha Rathnayake <Bawantharathnayake395@gmail.com>
2025-07-23 22:56:57 +0530

    update student dashboard

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ForgotPassword from './pages/loginPage/ForgotPassword';
 import ManageClassSchedules from './pages/dashboard/teacherDashboard/ManageClassSchedules';
 import HallAvailability from './pages/dashboard/teacherDashboard/HallAvailability';
 import ClassHalls from './pages/dashboard/adminDashboard/ClassHalls';
+import MyProfile from './pages/dashboard/studentDashboard/MyProfile';
 
 function App() {
   return (
@@ -29,6 +30,7 @@ function App() {
         <Route path="/register/institute" element={<InstituteRegister/>} />
         <Route path="/register/new" element={<NewStudentRegister/>} />
         <Route path="/forgotpassword" element={<ForgotPassword/>} />
+        <Route path="/student/profile" element={<MyProfile/>} />
       </Routes>
     </BrowserRouter>
   );
